08dabe373d26b5dd67a7af056b7d7534b8c205b2
pianoFF <fangfei1star@gmail.com>
2020-12-17 13:08:16 -0500

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,32 @@
 def second_challenge
+  # epic_tragedy = {
+  #   :montague => {
+  #     :patriarch => {},
+  #     :matriarch => {},
+  #     :hero => {},
+  #     :hero_friends => []
+  #   }, 
+  #   :capulet => {
+  #     :patriarch => {},
+  #     :matriarch => {},
+  #     :heroine => {},
+  #     :heroine_friends => []
+  #   }
+  # }
+
   epic_tragedy = {
-    :montague => {
-      :patriarch => {},
-      :matriarch => {},
-      :hero => {},
-      :hero_friends => []
-    }, 
-    :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
-    }
+    :montague => {},
+    :capulet => {}
   }
+  epic_tragedy[:montague][:patriarch] = {}
+  epic_tragedy[:montague][:matriarch] = {}
+  epic_tragedy[:montague][:hero] = {}
+  epic_tragedy[:montague][:hero_friends] = []
+  
+  epic_tragedy[:capulet][:patriarch] = {}
+  epic_tragedy[:capulet][:matriarch] = {}
+  epic_tragedy[:capulet][:heroine] = {}
+  epic_tragedy[:capulet][:heroine_friends] = []
+
+  epic_tragedy
 end
